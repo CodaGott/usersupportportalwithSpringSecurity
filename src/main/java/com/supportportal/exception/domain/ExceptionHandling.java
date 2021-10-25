ea16d122f9010024eb69435acfa394da34e8eb9e
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-10-25 15:32:54 +0100

    override the default error page

--- src/main/java/com/supportportal/exception/domain/ExceptionHandling.java
+++ src/main/java/com/supportportal/exception/domain/ExceptionHandling.java
@@ -3,6 +3,7 @@ package com.supportportal.exception.domain;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.supportportal.domain.HttpResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.persistence.NoResultException;
@@ -22,7 +24,8 @@ import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 @Slf4j
-public class ExceptionHandling {
+public class ExceptionHandling implements ErrorController {
+
     private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact admin";
     private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed at this endpoint. Please send a '%s' request";
     private static final String INTERNAL_SERVER_ERROR_MSG = "Error occurred while processing request";
@@ -30,6 +33,7 @@ public class ExceptionHandling {
     private static final String ACCOUNT_DISABLED = "Your account has been disabled. If this is an error, please contact admin";
     private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
     private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+    public static final String ERROR_PATH = "/error";
 
     @ExceptionHandler(DisabledException.class)
     public ResponseEntity<HttpResponse> accountDisabledException(){
@@ -82,6 +86,16 @@ public class ExceptionHandling {
         return createHttpResponse(METHOD_NOT_ALLOWED, String.format(METHOD_IS_NOT_ALLOWED, supportMethod));
     }
 
+    // TODO: 25 Oct 2021  // Below is one of the ways to handle or create a custom error handling for pages that does not exist
+    //  Instead of having the whitelabel error page. but this is not the best way as it will force use to
+    //  disable the NoHandlerFoundException class and this class has other functions that can benefit our app.
+
+
+// TODO: 25 Oct 2021   @ExceptionHandler(NoHandlerFoundException.class)
+//    ResponseEntity<HttpResponse> methodNotSupportedException(NoHandlerFoundException exception){
+//        return createHttpResponse(BAD_REQUEST, "Page not found");
+//    }
+
     @ExceptionHandler(Exception.class)
     ResponseEntity<HttpResponse> internalServerErrorException(Exception exception){
         log.error(exception.getMessage());
@@ -95,7 +109,7 @@ public class ExceptionHandling {
     }
 
     @ExceptionHandler(IOException.class)
-    ResponseEntity<HttpResponse> iOException(IOException exception){
+    public ResponseEntity<HttpResponse> iOException(IOException exception){
         log.error(exception.getMessage());
         return createHttpResponse(INTERNAL_SERVER_ERROR, ERROR_PROCESSING_FILE);
     }
@@ -103,6 +117,11 @@ public class ExceptionHandling {
     private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message){
         return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus,
                 httpStatus.getReasonPhrase().toUpperCase(), message.toUpperCase()), httpStatus);
+    }
+
+    @RequestMapping(ERROR_PATH)
+    ResponseEntity<HttpResponse> notFound404(IOException exception){
+        return createHttpResponse(NOT_FOUND, "This page does not exist");
     }
 
 }
--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -1,17 +1,19 @@
 package com.supportportal.resource;
 
 import com.supportportal.exception.domain.ExceptionHandling;
+import com.supportportal.exception.domain.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(path = {"/", "/user"})
 public class UserResource extends ExceptionHandling {
 
 
     @GetMapping("/getUser")
-    public String showUser(){
-        return "Hello World";
+    public String showUser() throws UserNotFoundException{
+//        return "Hello World";
+        throw new UserNotFoundException("User was not found");
     }
 }
